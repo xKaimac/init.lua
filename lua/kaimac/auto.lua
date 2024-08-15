e5668de180d51dd7dcf6d8eca3fac10e858852e3
xKaimac <admin@kaimac.com.au>
2024-08-16 08:27:58 +0930

    auto enter a newly made directory in netrw

--- lua/kaimac/auto.lua
+++ lua/kaimac/auto.lua
@@ -3,3 +3,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   command = "Prettier"
 })
+
+-- Auto enter a newly created directory
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        local function new_dir_and_enter()
+            local name = vim.fn.input("New directory name: ")
+            if name ~= "" then
+                local current_dir = vim.b.netrw_curdir
+                local target = current_dir .. '/' .. name
+                vim.fn.mkdir(target, "p")
+                vim.cmd('silent Explore ' .. vim.fn.fnameescape(target))
+            end
+        end
+
+        vim.keymap.set("n", "d", new_dir_and_enter, { buffer = true })
+    end,
+})
