459b73449ca9e93adf3a80c8d6e310569d2d5a13
xKaimac <admin@kaimac.com.au>
2024-08-16 13:46:24 +0930

    turned on smart case

--- lua/kaimac/set.lua
+++ lua/kaimac/set.lua
@@ -2,8 +2,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- Searches are case insensitive
+-- Searches are case insensitive until an uppercase char is used in the search
 vim.opt.ignorecase = true
+vim.opt.smartcse = true
 
 -- Tabs are 2 spaces
 vim.opt.tabstop = 2
